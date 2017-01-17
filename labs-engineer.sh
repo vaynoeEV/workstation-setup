350cc3d1c9f2234dbe9a8c7788e58d061f4d3fe1
Pivotal <pivotal@sterling.ny.pivotallabs.com>
2017-01-17 13:31:56 -0500

    Install global dependencies for vaynoe

--- labs-engineer.sh
+++ labs-engineer.sh
@@ -4,3 +4,12 @@ export SCRIPTS_DIR="$(dirname "$0")/scripts"
 ${SCRIPTS_DIR}/setup.sh labs-engineer
 
 brew install go
+brew install glide
+gem install foreman
+npm install -g ember-cli
+npm install -g bower
+
+brew install fontforge --with-python
+brew install eot-utils
+gem install fontcustom
+brew install rethinkdb
